781aea48d91ccc7e1a101df73da8a9e0a858f1c5
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-04-03 23:16:12 +0530

    Added files

---
+++ Web App/static/js/script.js
@@ -0,0 +1,5 @@
+document.getElementById("clear-btn").addEventListener("click", function() {
+    document.getElementById("source_code").value = "";
+    document.getElementById("output").innerText = "";
+  });
+    
