910eb9f2197299530ce9e00acfef97a4cb771431
kshitij7704 <kashyapkshitij7704@gmail.com>
2025-04-03 23:27:52 +0530

    Remove files

--- Web App/static/js/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("clear-btn").addEventListener("click", function() {
-    document.getElementById("source_code").value = "";
-    document.getElementById("output").innerText = "";
-  });
-    
